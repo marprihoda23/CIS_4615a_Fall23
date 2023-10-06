70c18879afe42e1690a74340ad7418cd3093d2f3
marprihoda23 <90920625+marprihoda23@users.noreply.github.com>
2023-10-06 16:41:03 -0400

    Wrote Noncompliant Code R07_ERR50_J.java

--- R07_ERR50_J.java
+++ R07_ERR50_J.java
@@ -2,3 +2,20 @@
  *Rule 07: Exceptional Behavior (ERR)
  *ERR50-J. Use exceptions only for exceptional conditions
  ******************************************************************************/
+public String processSingleString(String string){
+    // . . .
+    return string;
+  }
+public String processStrings(String[] strings){
+  String result = "";
+  int i = 0;
+  try{
+    while(true){
+      result = result.concat(processSingleString(string[i]));
+      i++;
+    }
+  }catch(ArrayIndexOutOfBoundsException e){
+    //ignore, we're done
+  }
+  return result;
+}
