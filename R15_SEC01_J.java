7f5ce4742ee7b038e4d101dd3f20bfc6523b294e
marprihoda23 <90920625+marprihoda23@users.noreply.github.com>
2023-10-08 15:55:37 -0400

    Noncompliant Code R15_SEC01_J.java

--- R15_SEC01_J.java
+++ R15_SEC01_J.java
@@ -1,1 +1,17 @@
-
+/******************************************************************************
+ *Rule 13: Input Output (FIO)
+ *FIO50-J. Do not make assumptions about file creation
+ ******************************************************************************/
+private void privilegedMethod(final String filename)throws FileNotFoundException {
+  try {
+    FileInputStream fis =(FileInputStream) AccessController.doPrivileged(new PrivilegedExceptionAction() {
+          public FileInputStream run() throws FileNotFoundException {
+              return new FileInputStream(filename);
+        }
+      }
+    );
+    //Do something with the file and then close it
+  } catch (PrivilegedActionException e) {
+    //Forward to handler
+  }
+}
