f539485e2a385c7cf2a0032424eb514b1ba23225
marprihoda23 <90920625+marprihoda23@users.noreply.github.com>
2023-10-08 17:10:03 -0400

    Compliant Solution R04_STR03_J.java

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -3,8 +3,9 @@
  *(FileName:R04_STR03_J.java) Do not encode noncharacter data as a string 
  *Given the non-compliant code
  ******************************************************************************/
-BigInteger x = new BigInteger ("530500452766");
-byte[]byteArray = x.toByteArray();
-String s = new String(byteArray);
-byteArray = s.getBytes();
-x = new BigInteger(byteArray);
+//Correct the code as shown in the Compliant Solution below:
+BigInteger x = new BigInteger("530500452766");
+String s = x.toString(); //Valid character data
+byte[]byteArray = s.getBytes();
+String ns = new String(byteArray);
+x = new BigInteger(ns);
