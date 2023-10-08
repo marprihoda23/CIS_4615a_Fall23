463b7388b858ad28d29f266ba3efd61f11859e63
marprihoda23 <90920625+marprihoda23@users.noreply.github.com>
2023-10-08 16:41:28 -0400

    Compliant Solution R02_XP00_J.java

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -2,8 +2,11 @@
  *Rule 02. Expressions (EXP)
  *(FileName:R02_XP00_J.java) Do not ignore values returned by methods Given the non-compliant code
  ******************************************************************************/
+//Correct the code as shown in theCompliant Solution below:
 public void deleteFile(){
     File someFile = new File("someFileName.txt");
     // Do something with someFile
-    someFile.delete();
+    if(!someFile.delete()){ 
+        //Handle failure to delete the file
+    }
 }
