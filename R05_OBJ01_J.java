3f115c048afa428ba76d447fff28b9a7f2a5432d
marprihoda23 <90920625+marprihoda23@users.noreply.github.com>
2023-10-08 16:26:30 -0400

    Compliant Soultion R05_OBJ01_J.java
    Compliant Solution (Private Primitive Field):
    Accessor methods provide controlled access to fields outside of the package in which their class is declared. This compliant solution declares total as private and provides a public accessor. The add() and remove() methods modify its value while preserving class invariants. 
    
    Accessor methods can perform additional functions, such as input validation and security manager checks, before manipulating the state.

--- R05_OBJ01_J.java
+++ R05_OBJ01_J.java
@@ -3,27 +3,12 @@
  *OBJ01-J: Limit accessibility of fields
  ******************************************************************************/
 
-/*In this noncompliant code example, the total field tracks the total number of elements as they are added to
-and removed from a container using the methods add() and remove() respectively. */
 public class Widget {
-  public int total; // Number of elements
+  private int total; // Declared private
  
-  void add() {
-    if (total < Integer.MAX_VALUE) {     
-      total++;
-      // ...
-    } else {
-      throw new ArithmeticException("Overflow");
-    }
+  public int getTotal () {
+    return total;
   }
  
-  void remove() { 
-    if (total > 0) {     
-      total--;
-      // ...
-    } else {
-      throw new ArithmeticException("Overflow");
-    }
-  }
+  // Definitions for add() and remove() remain the same
 }
-//As a public field, total can be altered by client code independently of the add() and remove() methods.
