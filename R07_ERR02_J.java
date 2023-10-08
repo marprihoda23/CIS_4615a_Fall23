a87d7ab331184dbafd24a5e3c5b53ee040ce34e4
marprihoda23 <90920625+marprihoda23@users.noreply.github.com>
2023-10-08 16:50:58 -0400

    Compliant Code R07_ERR02_J.java

--- R07_ERR02_J.java
+++ R07_ERR02_J.java
@@ -4,7 +4,7 @@
  ******************************************************************************/
 try {
   // ...
-} catch (SecurityException se) {
-  System.err.println(se);
+} catch(SecurityException se) {
+  logger.log(Level.SEVERE, se);
   // Recover from exception
 }
