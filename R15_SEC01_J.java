e106d7e55f66f7615ba1729dd3a80d8b334fbebd
marprihoda23 <90920625+marprihoda23@users.noreply.github.com>
2023-10-08 16:03:22 -0400

    Compliant Solution R15_SEC01_J.java
    This compliant solution invokes the cleanAFilenameAndPath() method to sanitize malicious inputs. Successful completion of the sanitization method indicates that the input is acceptable and the doPrivileged() block can be executed.

--- R15_SEC01_J.java
+++ R15_SEC01_J.java
@@ -3,15 +3,25 @@
  *FIO50-J. Do not make assumptions about file creation
  ******************************************************************************/
 private void privilegedMethod(final String filename)throws FileNotFoundException {
+  final String cleanFilename;
+  
   try {
-    FileInputStream fis =(FileInputStream) AccessController.doPrivileged(new PrivilegedExceptionAction() {
-          public FileInputStream run() throws FileNotFoundException {
-              return new FileInputStream(filename);
+    cleanFilename = cleanAFilenameAndPath(filename);
+  } catch (/* exception as per spec of cleanAFileNameAndPath */) {
+    // Log or forward to handler as appropriate based on specification
+    // of cleanAFilenameAndPath
+  }
+  
+    try{
+    FileInputStream fis =
+        (FileInputStream) AccessController.doPrivileged(
+          new PrivilegedExceptionAction() {
+        public FileInputStream run() throws FileNotFoundException {
+          return new FileInputStream(cleanFilename);
         }
       }
-    );
-    //Do something with the file and then close it
+    );// Do something with the file and then close it
   } catch (PrivilegedActionException e) {
-    //Forward to handler
+    // Forward to handler
   }
 }
