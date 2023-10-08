3b7acfb3cb4395500760383b92e587069c8e503d
marprihoda23 <90920625+marprihoda23@users.noreply.github.com>
2023-10-08 16:40:36 -0400

    Create R02_XP00_J.java

---
+++ R02_XP00_J.java
@@ -0,0 +1,9 @@
+/******************************************************************************
+ *Rule 02. Expressions (EXP)
+ *(FileName:R02_XP00_J.java) Do not ignore values returned by methods Given the non-compliant code
+ ******************************************************************************/
+public void deleteFile(){
+    File someFile = new File("someFileName.txt");
+    // Do something with someFile
+    someFile.delete();
+}
